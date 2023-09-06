3479b13ebe919d11083c1d71e5714e46354d9bc8
Yogini <yoginithakur01@gmail.com>
2023-09-06 21:23:30 +0530

    change in app.js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+// const express = require('express');
+// const app=express();
+
+// var port= 3000;
+
+// app.get('/',(req,res)=>res.send('Hello World'));
+// app.listen(port ,()=>console.log("Server is running on port:3000"));
+
+
 const express = require('express');
-const app=express();
+const app = express();
+const port = 3000; // You can change this to any port you prefer
 
-var port=process.env.PORT  ||3000;
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
 
-app.get('/',(req,res)=>res.send('Hello World'));
-app.listen(port ,()=>console.log("Server is running on port:3000"));
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
